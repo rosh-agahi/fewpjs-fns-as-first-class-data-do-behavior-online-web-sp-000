6cf49f8d98f5b7d933603ffc2a047aa1a51df4bd
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:11:37 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  if (parseInt(time) < 12) {
+    return 'Good Morning'
+  } else if(parseInt(time) >= 12 && parseInt(time) < 17){
+    return 'Good Afternoon'
+  } else if(parseInt(time) > 17 ){
+    return 'Good Evening'
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(testContent) {
+document.getElementById("greeting").innerText = testContent
+}
